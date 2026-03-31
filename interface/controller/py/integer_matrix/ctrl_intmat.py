3136aa0908bb4977b1f9aa987679d71fc3c96bb8
Mingyu Jeong <159212138+RFA0608@users.noreply.github.com>
2026-03-31 18:29:32 +0900

    Update ctrl_intmat.py

--- interface/controller/py/integer_matrix/ctrl_intmat.py
+++ interface/controller/py/integer_matrix/ctrl_intmat.py
@@ -1,6 +1,3 @@
-# get tcp_protocol description (vscode[debuger] launched at root directory)
-import sys
-sys.path.append(r"./communication/py")
 import tcp_protocol_client as tcc
 
 # init tcp host and port
